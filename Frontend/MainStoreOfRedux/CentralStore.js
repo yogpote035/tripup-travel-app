ebf2a623d132b3d3d7256e290a81c93a9b4e00d4
Yogesh Pote <yogpote035@gmail.com>
2025-08-06 00:33:25 +0530

    Solve path issue

--- Frontend/MainStoreOfRedux/CentralStore.js
+++ Frontend/MainStoreOfRedux/CentralStore.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../AllStatesFeatures/Authentication/authSlice";
 import trainReducer from "../AllStatesFeatures/Train/AllTrainsSlice";
 import bookingReducer from "../AllStatesFeatures/Train/BookTrainTicketSlice";
-import userProfileReducer from "../AllStatesFeatures/UserProfile/userProfileSlice";
+import userProfileReducer from "../AllStatesFeatures/UserProfile/UserProfileSlice";
 import busReducer from "../AllStatesFeatures/Bus/AllBusSlice";
 import BookBusReducer from "../AllStatesFeatures/Bus/BookBusTicketSlice";
 import flightReducer from "../AllStatesFeatures/Flight/AllFlightSlice";
