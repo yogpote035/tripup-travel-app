8124277133ccfd9bc30a5045f47b2415f9688785
Yogesh Pote <yogpote035@gmail.com>
2026-01-18 18:28:50 +0530

    refactor: Remove unused token verification middleware from refresh token route

--- Backend/routes/AuthenticationRoutes/index.js
+++ Backend/routes/AuthenticationRoutes/index.js
@@ -5,9 +5,10 @@ const {
   Login,
   generateRefreshToken,
 } = require("../../controllers/Authentication/UserController");
-const  verifyJWE  = require("../../Middleware/DecodeToken");
+const verifyJWE = require("../../Middleware/DecodeToken");
 router.post("/signup", Signup);
 router.post("/login", Login);
-router.post("/refresh-token", verifyJWE, generateRefreshToken);
+// router.post("/refresh-token", verifyJWE, generateRefreshToken);
+router.post("/refresh-token", generateRefreshToken);
 
 module.exports = router;
